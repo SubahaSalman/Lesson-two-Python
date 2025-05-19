ff4385c0d800db4674dd2cd673c13c66380796c7
SubahaSalman <subaha.salman@gmail.com>
2025-05-19 19:10:25 +0200

    Birthdays

---
+++ homework.py
@@ -0,0 +1,11 @@
+Dad="27/08/1975"
+Mom="09/02/1983"
+Me="26/01/2009"
+Sister="27/08/2010"
+Brother="24/10/2016"
+
+print("My dad's birthday is on:", Dad )
+print("My mom's birthday is on:", Mom )
+print("My birthday is on:", Me )
+print("My sister's birthday is on:", Sister )
+print("My brother's birthday is on:", Brother )
